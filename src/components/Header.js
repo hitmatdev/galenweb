8523380042d5303bdfcc5f7fd8fad5c89d1fc4b5
hmathpal <hmathpal@klick.com>
2023-06-26 17:00:44 -0400

    hubspot  commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,8 @@ import {
   XMarkIcon,
 } from '@heroicons/react/24/outline'
 import { ChevronDownIcon, DocumentChartBarIcon, PhoneIcon, PlayCircleIcon } from '@heroicons/react/20/solid'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image';
+import { Script } from "gatsby"
+
 
 const products = [
   { name: 'Data', description: 'Top data sources for healthcare data', href: '/product', icon: CircleStackIcon },
@@ -34,8 +35,12 @@ export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
 
   return (
-    <header className="relative z-50 w-full flex-none text-sm font-semibold leading-6 text-slate-900">
     
+ 
+    <header className="relative z-50 w-full flex-none text-sm font-semibold leading-6 text-slate-900">
+  
+  <script type="text/javascript" id="hs-script-loader" async defer src="//js-na1.hs-scripts.com/40091071.js"></script>
+
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5">
